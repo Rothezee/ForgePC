e79f7a5b65638c2b2200bbde8f4b4b3f7dc06161
Rothezee <rothalan83@gmail.com>
2026-06-08 16:01:02 -0300

    cambios menores

--- ejecutar.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /d "%~dp0"
-call compilar.bat
-if errorlevel 1 exit /b 1
-
-set "ABS_JAR=%USERPROFILE%\.m2\repository\org\netbeans\external\AbsoluteLayout\RELEASE290\AbsoluteLayout-RELEASE290.jar"
-"C:\Program Files\BellSoft\LibericaJDK-22\bin\java.exe" -cp "target\classes;%ABS_JAR%" Interfaz.Main
